01f59307f25bfecff4b1eb3f09038de7cfdc6f9a
Prabhashani mohotti <nadishkaprabhashani100@gmail.com>
2026-01-15 17:22:36 +0000

    Add project files.

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Device.Management.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Success");
+        }
+    }
+}
